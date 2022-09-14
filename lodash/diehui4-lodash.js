e69e586d069da67d0ed6d0e22468d02d335f5ac8
chenqihui <Aiden@example.com>
2022-09-14 21:21:43 +0800

    drop

--- lodash/diehui4-lodash.js
+++ lodash/diehui4-lodash.js
@@ -54,5 +54,12 @@ var diehui4 = {
       }
     }
     return result
+  },
+  drop: function (array, n = 1) {
+    var result = []
+    for (i = n; i < array.length; i++) {
+      result.push(array[i])
+    }
+    return result
   }
 }
