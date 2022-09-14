9e9d14062c8770993a3320570bf2377bfba754a0
chenqihui <Aiden@example.com>
2022-09-14 20:35:57 +0800

    js

--- lodash/diehui4-lodash.js
+++ lodash/diehui4-lodash.js
@@ -15,5 +15,42 @@ var diehui4 = {
         index++
       }
     }
+  },
+  compact: function (array) {
+    var result = []
+    for (var i = 0; i < array.length; i++) {
+      if (array[i]) {
+        result.push(array[i])
+      }
+    }
+    return result
+  },
+  concat: function () {
+    var result = arguments[0]
+    for (var i = 1; i < arguments.length; i++) {
+      if (Array.isArray(arguments[i])) {
+        for (var j = 0; j < arguments[i].length; j++) {
+          result.push(arguments[i][j])
+        }
+      } else {
+        result.push(arguments[i])
+      }
+    }
+    return result
+  },
+  difference: function (array, values) {
+    var result = []
+    for (var i = 0; i < array.length; i++) {
+      var istrue = true
+      for (var j = 0; j < values.length; j++) {
+        if (array[i] == values[j]) {
+          istrue = false
+        }
+      }
+      if (istrue) {
+        result.push(array[i])
+      }
+    }
+    return result
   }
 }
