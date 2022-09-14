60c3d86599cac16bd52dcf120eae32b821394fe2
chenqihui <Aiden@example.com>
2022-09-14 19:46:58 +0800

    chunk

--- lodash/diehui4-lodash.js
+++ lodash/diehui4-lodash.js
@@ -1,3 +1,19 @@
 var diehui4 = {
-
+  chunk: function (array, size) {
+    var result = []
+    var index = 0
+    for (var i = 0; i < array.length - 1; i++) {
+      if (!array[index]) {
+        return result
+      }
+      result[i] = []
+      for (var j = 0; j < size; j++) {
+        if (!array[index]) {
+          return result
+        }
+        result[i][j] = array[index]
+        index++
+      }
+    }
+  }
 }
